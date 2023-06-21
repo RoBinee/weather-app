9254f67ceb948533c0f5d1ec1953e80c529f51df
Robinee <yb6925@naver.com>
2023-06-21 13:35:06 +0900

    Remove HTMLWebpackPlugin to hard code form in HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,12 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: 'Weather-app',
-    }),
-  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
