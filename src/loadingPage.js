5bceff4d21162154bf580a4e49531a421e569d46
Robinee <yb6925@naver.com>
2023-06-23 20:03:33 +0900

    Load loader.gif using webpack

--- src/loadingPage.js
+++ src/loadingPage.js
@@ -1,7 +1,8 @@
+import MyImage from './loader.gif';
+
 const loadingPage = () => {
   const gif = new Image();
-  gif.src =
-    'https://cdn.pixabay.com/animation/2022/11/04/09/42/09-42-03-510_512.gif';
+  gif.src = MyImage;
   return gif;
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      // {
-      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //   type: 'asset/resource',
-      // },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
