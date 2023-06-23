b8424685c0214b7a22dd4588ca92ebf51c1332ae
Robinee <yb6925@naver.com>
2023-06-23 20:46:16 +0900

    Make content structure

--- src/displayContent.js
+++ src/displayContent.js
@@ -1,9 +1,9 @@
 const displayContent = (obj) => {
   const { name, date, weather, icon, temp, humidity } = obj;
 
-  console.log(obj);
-
-  const string = `<h1>${name}! ${date}, ${weather},${temp}, ${humidity}</h1><img src=${icon} />`;
+  const string = `<section>
+  <h1>${name}</h1><img src=${icon} /><h1>${weather}</h1><h3>${temp}°C</h3><p>humidity ${humidity}</p><p>${date}</p>
+  </section>`;
   return string;
 };
 
