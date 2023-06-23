94fff893b028c715531ca0d31f006eede5152b6f
Robinee <yb6925@naver.com>
2023-06-23 20:37:08 +0900

    Unstructure location and use it for country name

--- src/arrangeData.js
+++ src/arrangeData.js
@@ -1,10 +1,19 @@
-const arrangeData = ({ current }) => {
+const arrangeData = (obj) => {
+  const { current, location } = obj;
   /**Use condition.text(weather desc), condition.icon, last_updated(date), temp_c, humidity*/
   const { condition, last_updated, temp_c, humidity } = current;
   const { text, icon } = condition;
+  const { country } = location;
 
   //return the arranged data object
-  return { date: last_updated, weather: text, icon, temp: temp_c, humidity };
+  return {
+    name: country,
+    date: last_updated,
+    weather: text,
+    icon,
+    temp: temp_c,
+    humidity,
+  };
 };
 
 export default arrangeData;
--- src/displayContent.js
+++ src/displayContent.js
@@ -1,6 +1,10 @@
 const displayContent = (obj) => {
-  const { date, weather, icon, temp, humidity } = obj;
-  container.innerHTML = `<h1>${date}, ${weather},${temp}, ${humidity}</h1><img src=${icon} />`;
+  const { name, date, weather, icon, temp, humidity } = obj;
+
+  console.log(obj);
+
+  const string = `<h1>${name}! ${date}, ${weather},${temp}, ${humidity}</h1><img src=${icon} />`;
+  return string;
 };
 
 export default displayContent;
