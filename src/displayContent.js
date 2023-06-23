27f54a093b1bbcb4dae5955ebb3773877df1954d
Robinee <yb6925@naver.com>
2023-06-23 21:35:52 +0900

    Rewrite a string for css

--- src/displayContent.js
+++ src/displayContent.js
@@ -1,7 +1,7 @@
 const displayContent = (obj) => {
   const { name, date, weather, icon, temp, humidity } = obj;
 
-  const string = `<section>
+  const string = `<section class="card">
   <h1>${name}</h1><img src=${icon} /><h1>${weather}</h1><h3>${temp}°C</h3><p>humidity ${humidity}</p><p>${date}</p>
   </section>`;
   return string;
